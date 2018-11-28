026ef32ce7f48c8de4846f056392a67cf2ffae0f
Nik Bornemeier <NikolausRB@gmail.com>
2018-11-28 15:42:16 -0700

    add functionality to parse and trum url paths

--- index.js
+++ index.js
@@ -6,12 +6,26 @@
 
 // Dependencies
 
-const http = require('http')
+const http = require('http');
+const url = require('url');
 
 //The servier should respond to all requests with astring.
 
 const server = http.createServer((request, response)=>{
-  response.end('Hello World\n')
+
+  //Get url and parse it
+  const parsedUrl = url.parse(request.url, true);
+
+  //Get path from url
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  //send the responsen
+  response.end('Hello World\n');
+
+  //console log the request path
+  console.log('Request received on path: '+trimmedPath)
+
 })
 
 //Start the server and have it listening on port 300.
